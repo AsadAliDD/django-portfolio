b8fb82d31bb080dd28b3ef6a1d0d42cc9d033567
Asad <asadalisps@gmail.com>
2019-03-31 17:45:09 +0500

    Changed Database and added Blogs Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+
+    blog_id = models.IntegerField();
+    title = models.CharField(max_length=100)
+    desciption = models.CharField(max_length=400)
