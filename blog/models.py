f2f90b0e19e5694d3ad040b05c47bae7098b9a44
Asad <asadalisps@gmail.com>
2019-04-01 21:08:01 +0500

    Jobs +Bootstrap

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
 
-    blog_id = models.IntegerField();
     title = models.CharField(max_length=100)
-    desciption = models.CharField(max_length=400)
+    desciption = models.TextField()
+    date = models.DateTimeField()
+    image = models.ImageField(upload_to='images/',default='images/default.png')
